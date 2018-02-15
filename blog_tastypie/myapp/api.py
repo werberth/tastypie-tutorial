363c906c387a3c8dd1ef6e158d4ca8c0b6523dd1
Werberth Vinícius <werberthvinicius@gmail.com>
2018-02-15 19:49:03 -0300

    Exclude unnecessary fields

--- blog_tastypie/myapp/api.py
+++ blog_tastypie/myapp/api.py
@@ -8,6 +8,10 @@ class UserResource(ModelResource):
     class Meta:
         queryset = User.objects.all()
         resource_name = 'user'
+        excludes = [
+            'email', 'password',
+            'is_active', 'is_staff', 'is_superuser'
+        ]
 
 
 class EntryResource(ModelResource):
