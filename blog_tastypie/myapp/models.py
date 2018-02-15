3c0ca46c945cde7289cb2ca4bdf16d9489b4d985
Werberth Vinícius <werberthvinicius@gmail.com>
2018-02-15 17:22:12 -0300

    Adding Entry model

--- blog_tastypie/myapp/models.py
+++ blog_tastypie/myapp/models.py
@@ -1,3 +1,12 @@
+from tastypie.utils.timezone import now
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Entry(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField(default=now)
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(null=True, blank=True)
+    body = models.TextField()
